d8391665ce7d9b803bfd9a769b249438790c3bb4
Delano Coleman <N00242335@iadt.ie>
2025-10-13 13:09:43 +0100

    test 2

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ClubController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. Test
      */
     public function index()
     {
