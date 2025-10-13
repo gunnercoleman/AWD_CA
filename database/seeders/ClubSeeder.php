d3a3680773614689a262ce2b7924b434efd1b86f
Delano Coleman <N00242335@iadt.ie>
2025-10-13 13:08:47 +0100

    Test

---
+++ database/seeders/ClubSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class ClubSeeder extends Seeder{
+    public function run(): void{
+        
+        $currentTimestamp = Carbon::now();
+
+        Club::insert([
+            [
+                'name' => 'Arsenal',
+                'position' => 2,
+                'description' => 'Best club in London',
+                'image' => 'arsenal.jpg' 
+            ],
+
+            [
+                'name' => 'Bayern Munich',
+                'position' => 1,
+                'description' => 'Best club in Germany',
+                'image' => 'bayernmunich.jpg' 
+            ],
+
+            [
+                'name' => 'Inter Milan',
+                'position' => 3,
+                'description' => 'Mid club in Italy',
+                'image' => 'intermilan.jpg' 
+            ],
+
+            [
+                'name' => 'Tottenham',
+                'position' => 16,
+                'description' => 'Below average team in London',
+                'image' => 'intermilan.jpg' 
+            ],
+                        [
+                'name' => 'Barcelona',
+                'position' => 2,
+                'description' => 'Best club in Spain',
+                'image' => 'intermilan.jpg' 
+            ],
+        ]);
+    }
+}
